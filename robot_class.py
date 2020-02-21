837dbf4918a744e242708c99019c8bc3eb258fd1
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-02-21 18:37:09 +0200

    Implement sense function

--- robot_class.py
+++ robot_class.py
@@ -18,13 +18,12 @@ import random
 #
 class robot:
     
-    # --------
-    # init:
-    #   creates a robot with the specified parameters and initializes
-    #   the location (self.x, self.y) to the center of the world
-    #
     def __init__(self, world_size = 100.0, measurement_range = 30.0,
-                 motion_noise = 1.0, measurement_noise = 1.0):
+                 motion_noise = 1.0, measurement_noise = 1.0):	
+        """
+        Creates a robot with the specified parameters and initializes 
+        the location (self.x, self.y) to the center of the world.
+        """
         self.measurement_noise = 0.0
         self.world_size = world_size
         self.measurement_range = measurement_range
@@ -34,22 +33,23 @@ class robot:
         self.measurement_noise = measurement_noise
         self.landmarks = []
         self.num_landmarks = 0
+        
     
-    
-    # returns a positive, random float
     def rand(self):
+        """
+        Returns a positive, random float.
+        """
         return random.random() * 2.0 - 1.0
     
-    
-    # --------
-    # move: attempts to move robot by dx, dy. If outside world
-    #       boundary, then the move does nothing and instead returns failure
-    #
+
     def move(self, dx, dy):
-        
+        """
+        Attempts to move robot by dx, dy. If outside world boundary, 
+        then the move does nothing and instead returns failure.
+        """
         x = self.x + dx + self.rand() * self.motion_noise
         y = self.y + dy + self.rand() * self.motion_noise
-        
+
         if x < 0.0 or x > self.world_size or y < 0.0 or y > self.world_size:
             return False
         else:
@@ -58,30 +58,26 @@ class robot:
             return True
 
 
-    # --------
     # sense: returns x- and y- distances to landmarks within visibility range
     #        because not all landmarks may be in this range, the list of measurements
-    #        is of variable length. Set measurement_range to -1 if you want all
-    #        landmarks to be visible at all times
+    #        is of variable length. Set measurement_range to -1 to make all
+    #        landmarks visible at all times
     #
     
-    ## TODO: paste your complete the sense function, here
-    ## make sure the indentation of the code is correct
     def sense(self):
-        ''' This function does not take in any parameters, instead it references internal variables
-            (such as self.landamrks) to measure the distance between the robot and any landmarks
-            that the robot can see (that are within its measurement range).
-            This function returns a list of landmark indices, and the measured distances (dx, dy)
-            between the robot's position and said landmarks.
-            This function should account for measurement_noise and measurement_range.
-            One item in the returned list should be in the form: [landmark_index, dx, dy].
-            '''
-           
+        """ 
+        This function does not take in any parameters, instead it references internal variables
+        (such as self.landamrks) to measure the distance between the robot and any landmarks
+        that the robot can see (that are within its measurement range). The function accounts 
+        for measurement_noise and measurement_range.
+
+        Returns a list of landmark indices, and the measured distances (dx, dy) between the robot's 
+        position and said landmarks. One item in the list will be in the form: [landmark_index, dx, dy].
+        """
+            
         measurements = []
-        
-        ## TODO: iterate through all of the landmarks in a world
-        
-        ## TODO: For each landmark
+
+        ## For each landmark
         ## 1. compute dx and dy, the distances between the robot and the landmark
         ## 2. account for measurement noise by *adding* a noise component to dx and dy
         ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
@@ -89,25 +85,37 @@ class robot:
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
-        
-        ## TODO: return the final, complete list of measurements
+
+        for i, lk in enumerate(self.landmarks):
+            dx = lk[0] - self.x
+            dy = lk[1] - self.y
+            dx += self.rand()*self.measurement_noise
+            dy += self.rand()*self.measurement_noise
+            d = sqrt(dx*dx + dy*dy)
+            
+            if d <= self.measurement_range:
+                measurements += [[i, dx, dy]]
+                #measurements += [(i, dx, dy)]
+
         return measurements
 
 
-    # --------
-    # make_landmarks:
-    # make random landmarks located in the world
-    #
     def make_landmarks(self, num_landmarks):
+        """
+        Make random landmarks located in the world.
+        """
         self.landmarks = []
         for i in range(num_landmarks):
             self.landmarks.append([round(random.random() * self.world_size),
-                                   round(random.random() * self.world_size)])
+                                    round(random.random() * self.world_size)])
         self.num_landmarks = num_landmarks
 
 
-    # called when print(robot) is called; prints the robot's location
+    
     def __repr__(self):
+        """
+        Called when print(robot) is called; prints the robot's location.
+        """
         return 'Robot: [x=%.5f y=%.5f]'  % (self.x, self.y)
 
 
