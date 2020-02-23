b27e97f3b9afe47443492007266f1adc019f7d43
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-02-23 17:00:58 +0200

    Describe Omega and Xi

--- robot_class.py
+++ robot_class.py
@@ -81,7 +81,6 @@ class robot:
         ## 1. compute dx and dy, the distances between the robot and the landmark
         ## 2. account for measurement noise by *adding* a noise component to dx and dy
         ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
-        ##    - Feel free to use the function self.rand() to help calculate this noise component
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
