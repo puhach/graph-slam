e07d79c117291d88784c9c9a26a5a417bb59329f
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-03-02 17:20:53 +0200

    Move landmark creation to the World's constructor

--- cpp-console/world.cpp
+++ cpp-console/world.cpp
@@ -1,2 +1,74 @@
 #include "world.h"
 #include "robot.h"
+
+#include <iterator>
+#include <vector>
+#include <random>
+
+/*
+using LandmarkList = std::vector<std::tuple<int, int> >;
+
+
+LandmarkList create_landmarks(int n, int worldHSize, int worldVSize)
+{
+    LandmarkList landmarks(n);
+
+    // Seed with a real random value, if available
+    std::random_device r;
+
+    // Choose a random mean between 1 and 6
+    //std::default_random_engine e1(r());
+    std::mt19937 gen(r());
+    std::uniform_int_distribution<int> uniform_dist;
+
+    for (auto& [x, y] : landmarks)
+    {
+        x = uniform_dist(gen) % worldHSize;
+        y = uniform_dist(gen) % worldVSize;
+    }
+
+    return landmarks;
+}
+*/
+
+
+
+World::World(int width, int height, int nLandmarks)
+    : width(width)
+    , height(height)
+    , landmarks(nLandmarks)
+{
+
+
+    // Seed with a real random value, if available
+    std::random_device r;
+
+    // Choose a random mean between 1 and 6
+    //std::default_random_engine e1(r());
+    std::mt19937 gen(r());
+    std::uniform_int_distribution<int> uniform_dist;
+
+    for (auto& [x, y] : this->landmarks)
+    {
+        x = uniform_dist(gen) % width;
+        y = uniform_dist(gen) % height;
+    }
+
+    /*
+    enum class World : int
+    {
+        rows = 10,
+        cols = 8
+    };
+
+    int nLandmarks = 12;
+    std::vector<std::tuple<int, int> > landmarks = create_landmarks(nLandmarks, static_cast<int>(World::cols), static_cast<int>(World::rows));
+
+    //std::copy(landmarks.begin(), landmarks.end(), std::ostream_iterator<std::tuple<int, int> >(std::cout, " "));
+    for (const auto& [x, y] : landmarks)
+        std::cout << "(" << x << ", " << y << ") ";
+    std::cout << std::endl;
+    */
+
+}
+
--- cpp-console/world.h
+++ cpp-console/world.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#include <vector>
 
 class World
 {
-};
+public:
+	World(int width, int height, int nLandmarks);
+
+private:
+	int width, height;
+	std::vector<std::tuple<int, int> > landmarks;
+};	// World
 
