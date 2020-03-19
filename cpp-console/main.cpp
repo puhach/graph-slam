c06a8c20b768a350716e73193ff1ffc682393db8
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-03-19 15:42:03 +0200

    Print actual robot positions after the moveAndSense loop

--- cpp-console/main.cpp
+++ cpp-console/main.cpp
@@ -18,27 +18,41 @@ int main()
     {
         // TODO: enter the world size, the number of landmarks, and the number of time steps
 
-        // initialize the world of size 8 x 10 with 12 landmarks
+        // Initialize the world of size 8 x 10 with 12 landmarks.
         World world(8, 10, 2);
 
         
-        // show the landmarks
+        // Show the landmarks.
         std::cout << world << std::endl;
 
         
-        // create the robot at position (3; 4) in the world
-        //Robot& robot = world.getRobot();
-        Robot robot(3, 4, 2, 1, 1.0, 1.0, world);
+        // Create the robot at position (3; 4) in the world.
+        Robot robot(3, 4, 2, 1, 0.8, 0.8, world);
 
-        /*
-        // simulate robot motions and measurements over 50 time steps
-        auto [measurements, displacements] = robot.moveAndSense(50);
+        
+        // Simulate robot motions and measurements over 50 time steps.
+        robot.moveAndSense(50);
+        //auto [measurements, displacements] = robot.moveAndSense(50);
         //robot.moveAndSense(50);
         //auto [motions, measurements] = robot.getHistory();
         //std::cout << robot << std::endl;
 
-        // TODO: print actual robot positions
+        // Print actual robot positions.
+        // In this implementation we assume that the initial position is known and the first displacement 
+        // equals to the distance from the world origin.
+        // TODO: try not to use the initial position info in the displacement history.
+        std::cout << "Actual robot positions: " << std::endl;
+        double x = 0, y = 0;
+        //for (const auto& d : displacements)
+        for (const auto &d : robot.getDisplacements())
+        {
+            x += d.first;
+            y += d.second;
 
+            std::cout << x << " " << y << std::endl;
+        }
+
+        /*
         // run SLAM to estimate positions of the robot and landmarks
         auto [estPositions, estLandmarks] = robot.localize();
         //auto [mux, muy] = slam(data);
